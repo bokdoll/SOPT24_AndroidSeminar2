61da51bf6e03601af6565502ee90a83b3d88f610
Lee Hyunjoo <winnerhj1109@naver.com>
2019-06-08 17:39:12 +0900

    after editting git config

---
+++ app/src/main/java/org/sopt24/dshyun0226/androidseminar/Data/ProductOverviewData.kt
@@ -0,0 +1,5 @@
+package org.sopt24.dshyun0226.androidseminar.Data
+
+data class ProductOverviewData(var thumnail: String, var idx: Int, var title: String, var num_like: Int, var author: String,  var isFinished: Int)
+
+
